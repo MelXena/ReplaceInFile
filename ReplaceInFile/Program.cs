49b514981bb8d08cb0fdbd7cfa4e95cc7b7f4fe9
Michael Elofson <mel@xenanetworks.com>
2018-11-12 16:54:30 +0100

    Add project files.

---
+++ ReplaceInFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
