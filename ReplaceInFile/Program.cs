99978aab1876eb70094084b5db618a87a24ae726
Michael Elofson <mel@xenanetworks.com>
2018-11-13 09:42:21 +0100

    Added error handling etc

--- ReplaceInFile/Program.cs
+++ ReplaceInFile/Program.cs
@@ -11,14 +11,33 @@ namespace ReplaceInFile
     {
         static void Main(string[] args)
         {
-            UpdateSettingsFile();
+            string resultText;
+            if (!SearchReplaceInFile(@"..\..\ValkyrieManager_settings.txt", @"\Xena\XenaManager-2G\", @"\Xena\ValkyrieManager\", true, out resultText))
+                throw new Exception(resultText);
         }
 
-        static void UpdateSettingsFile()
+        /// <summary>
+        /// Replaces all occurrences of oldString with newString in filename.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="oldString"></param>
+        /// <param name="newString"></param>
+        /// <param name="clearAttributes">If true then all file attributes for filename are cleared before processing.</param>
+        /// <param name="resultText">Contains an error description on failure.</param>
+        /// <returns>true if successful.</returns>
+        static bool SearchReplaceInFile(string filename, string oldString, string newString, bool clearAttributes, out string resultText)
         {
-            string str = File.ReadAllText(@"..\..\ValkyrieManager_settings.txt");
-            str = str.Replace(@"\\Xena\\XenaManager-2G\\", @"\\Xena\\ValkyrieManager\\");
-            File.WriteAllText(@"..\..\ValkyrieManager_settings.txt", str);
+            try {
+                string str = File.ReadAllText(filename);
+                str = str.Replace(oldString, newString);
+                File.WriteAllText(filename, str);
+            } catch (Exception exc) {
+                resultText = $"SearchReplaceInFile: Failed to replace '{oldString}' with '{newString}' in file '{filename}'.\n{exc.Message}";
+                return false;
+            }
+
+            resultText = "";
+            return true;
         }
 
     }
